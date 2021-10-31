10021b9c679f30a91616c5fc613ae4b9c18de81f
fahrizayusefa <83055248+FahrizaYusefa@users.noreply.github.com>
2021-10-31 22:23:33 +0700

    Add files via upload

---
+++ tampil_combo.php
@@ -0,0 +1,31 @@
+<table border='1'>
+    <tr>
+        <th>No</th>
+        <th>NIM</th>
+        <th>Nama</th>
+        <th>Prodi</th>
+        <th>Angkatan</th>
+    </tr>
+    <?php
+        include "koneksi.php";
+        if(isset($_POST['prodi'])){
+            $prodi=$_POST['prodi'];
+        }else {
+            $prodi='';
+        }
+
+        $hasil=mysqli_query($kon,"select * from mahasiswa where prodi='".$prodi."'order by nim asc");
+        $no=0;
+        while($data=mysqli_fetch_array($hasil));
+            $no++;
+    ?>
+
+    <tr>
+        <td><?php echo $no; ?></td>
+        <td><?php echo $data['NIM']; ?></td>
+        <td><?php echo $data['Nama']; ?></td>
+        <td><?php echo $data['Prodi']; ?></td>
+        <td><?php echo $data['Angkatan']; ?></td>
+    </tr>
+
+</table>
